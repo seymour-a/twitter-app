016f8424af74910706b70c3cc3cb88b9db656aa3
Seymur Ahmadzada <seymur.ahmadzada@gmail.com>
2024-10-29 14:13:19 +0400

    thymeleaf to mustache

--- src/main/java/com/twitter/twitterapp/TwitterAppApplication.java
+++ src/main/java/com/twitter/twitterapp/TwitterAppApplication.java
@@ -2,12 +2,14 @@ package com.twitter.twitterapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ThymeleafAutoConfiguration.class})
 public class TwitterAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwitterAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(TwitterAppApplication.class, args);
+    }
 
 }
